0a319d938f8659309a853b7e2a0bc9d53d44b431
zak201 <z.anouar832@gmail.com>
2025-01-13 10:26:10 +0100

    création du backoffice / problème d'identification à resoudre

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        // redirige vers une autre page ou affiche le tableau de bord
+        return $this->render('admin/dashboard.html.twig');
+    }
+}
