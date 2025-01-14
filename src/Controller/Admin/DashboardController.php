6c0367f086751debe0db521d6a4369ac6ac66fc4
zak201 <z.anouar832@gmail.com>
2025-01-14 16:24:14 +0100

    création complet du backoffice avec les entités. erreur de login à resoudre

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Utilisateur;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Controller\Admin\UtilisateurCrudController;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -13,5 +16,12 @@ class DashboardController extends AbstractDashboardController
     {
         // redirige vers une autre page ou affiche le tableau de bord
         return $this->render('admin/dashboard.html.twig');
+    }
+    public function configureMenuItems(): iterable
+    {
+        return [
+            MenuItem::linkToDashboard('Dashboard', 'fa fa-home'),
+            MenuItem::linkToCrud('Utilisateurs', 'fa fa-users', Utilisateur::class),
+        ];
     }
 }
